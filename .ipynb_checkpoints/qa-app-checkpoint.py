2449f571cad490b6af9fc5e9ec0d4866cb006393
Junichiro Iwasawa <junichiro.kid@gmail.com>
2023-08-12 12:10:49 +0900

    fix bug for load_youtube

--- .ipynb_checkpoints/qa-app-checkpoint.py
+++ .ipynb_checkpoints/qa-app-checkpoint.py
@@ -17,6 +17,8 @@ from langchain.vectorstores import Chroma, Qdrant
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
 
 def load_youtube(url: str, save_dir="docs/youtube/"):
     loader = GenericLoader(
@@ -27,8 +29,8 @@ def load_youtube(url: str, save_dir="docs/youtube/"):
     return docs
 
 
-def summarize_doc(docs, openai_api_key):
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k", openai_api_key=openai_api_key)
+def summarize_doc(docs):
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
     try:
         chain = load_summarize_chain(llm, chain_type="stuff")
         summary = chain.run(docs)
@@ -38,8 +40,8 @@ def summarize_doc(docs, openai_api_key):
     return summary
 
 
-def create_vectordb_for_docs(docs, openai_api_key, db="qdrant"):
-    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+def create_vectordb_for_docs(docs, db="qdrant"):
+    embedding = OpenAIEmbeddings()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 1500,
         chunk_overlap = 150
@@ -63,13 +65,13 @@ def create_vectordb_for_docs(docs, openai_api_key, db="qdrant"):
     return vectordb
 
 
-def init_llm(openai_api_key):
+def init_llm():
     current_date = datetime.datetime.now().date()
     if current_date < datetime.date(2023, 9, 2):
         llm_name = "gpt-3.5-turbo-0301"
     else:
         llm_name = "gpt-3.5-turbo"
-    llm = ChatOpenAI(model_name=llm_name, temperature=0, openai_api_key=openai_api_key)
+    llm = ChatOpenAI(model_name=llm_name, temperature=0)
     return llm
 
 
@@ -79,17 +81,15 @@ st.title("QA youtube transcription")
 
 url = st.text_input("Enter YouTube url:", placeholder="https://www.youtube.com/watch?v=nM_3d37lmcM")
 question = st.text_input("Enter your question:", placeholder="What does Schulman think is important for the future of ChatGPT?", disabled=not url)
-openai_api_key = st.text_input('OpenAI API Key', type='password')
 
 result = []
 with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not(url and question and openai_api_key))
+    submitted = st.form_submit_button('Submit', disabled=not(url and question))
     if submitted:
-        with st.spinner('Transcribing... This might take a few minutes.'):
-            openai.api_key = openai_api_key
+        with st.spinner('Transcribing...'):
             docs = load_youtube(url)
-            llm = init_llm(openai_api_key)  # init gpt-3.5-turbo
-            vectordb = create_vectordb_for_docs(docs, openai_api_key)
+            llm = init_llm()  # init gpt-3.5-turbo
+            vectordb = create_vectordb_for_docs(docs)
             retriever=vectordb.as_retriever()
             memory = ConversationBufferMemory(
                 memory_key="chat_history",
@@ -102,7 +102,5 @@ with st.form('myform', clear_on_submit=True):
             )
             response = qa({"question": question})
             result.append(response)
-            #summary = summarize_doc(docs, openai_api_key)
 if len(result):
     st.info(response["answer"])
-    #st.info(summary)
