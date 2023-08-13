465ca8305211d105d24aea9ccbbb4c78011642b0
Junichiro Iwasawa <junichiro.kid@gmail.com>
2023-08-13 13:52:57 +0900

    add conversational qa and sources

--- qa-app.py
+++ qa-app.py
@@ -2,10 +2,14 @@ import datetime
 import openai
 import os
 import streamlit as st
+import tempfile
+import uuid
 
+from openai.error import InvalidRequestError
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders.generic import GenericLoader
 from langchain.document_loaders.parsers import OpenAIWhisperParser
 from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
@@ -16,6 +20,12 @@ from langchain.vectorstores import Chroma, Qdrant
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
+
+
+def load_pdf(pdf):
+    loader = PyPDFLoader(pdf)
+    docs = loader.load()
+    return docs
 
 
 def load_youtube(url: str, save_dir="docs/youtube/"):
@@ -73,36 +83,107 @@ def init_llm(openai_api_key):
     return llm
 
 
-st.set_page_config(page_title="QA youtube transcription")
-st.title("QA youtube transcription")
+def init_qa_retriever(docs, openai_api_key):
+    llm = init_llm(openai_api_key)  # init gpt-3.5-turbo
+    vectordb = create_vectordb_for_docs(docs, openai_api_key)
+    retriever=vectordb.as_retriever(search_type="mmr")
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    qa = ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever=retriever,
+        memory=memory,
+    )
+    return qa, retriever
 
 
-url = st.text_input("Enter YouTube url:", placeholder="https://www.youtube.com/watch?v=nM_3d37lmcM")
-question = st.text_input("Enter your question:", placeholder="What does Schulman think is important for the future of ChatGPT?", disabled=not url)
-openai_api_key = st.text_input('OpenAI API Key', type='password')
+def repeat_qa(qa):
+    q_x = st.text_input(
+        "Enter your question:",
+        placeholder="Is there any information about Y?",
+    )
+    with st.form("form_x", clear_on_submit=False):
+        q_submitted = st.form_submit_button("Submit", disabled=not q_x)
+        res = []
+        if q_submitted:
+            with st.spinner("Asking question"):
+                response_x = qa({"question": question})
+                res.append(response_x)
+        if len(res):
+            st.info(response_x["answer"])
+            repeat_qa(qa)  # StreamlitAPIException: Forms cannot be nested in other forms.
 
-result = []
-with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not(url and question and openai_api_key))
+
+st.set_page_config(page_title="QA_youtube_pdf")
+st.title("QA with YouTube or PDF")
+
+url = st.text_input(
+    "Enter YouTube url (preprocessing might take a few minutes):",
+    placeholder="https://www.youtube.com/watch?v=nM_3d37lmcM",
+)
+pdf = st.file_uploader("Upload an article:", type="pdf")
+spinner_msg = (
+    "Downloading & transcribing the video... This might take a few minutes." 
+    if url else "Processing pdf"
+)
+
+if "api_key" not in st.session_state:
+    try:
+        st.session_state.api_key = os.environ['OPENAI_API_KEY']
+    except KeyError:
+        st.session_state.api_key = st.text_input('OpenAI API Key', type='password')
+
+with st.form("upload_form", clear_on_submit=False):
+    disable_cond = ("docs" in st.session_state) or not((url or pdf) and st.session_state.api_key)
+    submitted = st.form_submit_button("Preprocess document", disabled=disable_cond)
+    summary_storage = []
     if submitted:
-        openai.api_key = openai_api_key
-        with st.spinner("Downloading & transcribing the video... This might take a few minutes."):
-            docs = load_youtube(url)
-            #llm = init_llm(openai_api_key)  # init gpt-3.5-turbo
-            #vectordb = create_vectordb_for_docs(docs, openai_api_key)
-            #retriever=vectordb.as_retriever()
-            #memory = ConversationBufferMemory(
-            #    memory_key="chat_history",
-            #    return_messages=True
-            #)
-            #qa = ConversationalRetrievalChain.from_llm(
-            #    llm,
-            #    retriever=retriever,
-            #    memory=memory,
-            #)
-            #response = qa({"question": question})
-            #result.append(response)
-            summary = summarize_doc(docs, openai_api_key)
-if len(result):
-    #st.info(response["answer"])
-    st.info(summary)
+        openai.api_key = st.session_state.api_key
+        with st.spinner(spinner_msg):
+            if pdf:
+                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                    tmp_file.write(pdf.getvalue())
+                    tmp_file_path = tmp_file.name
+                    st.session_state.docs = load_pdf(tmp_file_path)
+            elif url:
+                st.session_state.docs = load_youtube(url)
+
+if "summary" in st.session_state:
+    st.info("Summary: " + st.session_state.summary)
+else:
+    with st.form("summary_form", clear_on_submit=False):
+        summary_required = st.form_submit_button("Summarize document", disabled=not((url or pdf) and st.session_state.api_key and ("docs" in st.session_state)))
+        if summary_required:
+            with st.spinner("Summarizing... (might take a few minutes)"):
+                st.session_state.summary = summarize_doc(st.session_state.docs, st.session_state.api_key)
+            st.info(st.session_state.summary)
+if "responses" not in st.session_state:
+    #st.session_state.unique_ids = []
+    st.session_state.responses = []
+    st.session_state.sources = []
+else:
+    for response in st.session_state.responses:
+        st.info("Q. " + response["question"])
+        st.info("A. " + response["answer"])
+
+question = st.text_input(
+    "Enter your question:",
+    placeholder="Is there any information about X?",
+)
+with st.form("question_form", clear_on_submit=True):
+    q_submitted = st.form_submit_button("Ask question", disabled=not (question and ("docs" in st.session_state)))
+    if q_submitted:
+        with st.spinner("Asking question"):
+            qa, retriever = init_qa_retriever(st.session_state.docs, st.session_state.api_key)
+            response = qa({"question": question})
+            st.session_state.responses.append(response)
+            st.session_state.sources.append(retriever.get_relevant_documents(query=question))
+        
+        st.info("A. " + st.session_state.responses[-1]["answer"])
+        st.info("Sources for answering (1): \n" + st.session_state.sources[-1][0].page_content)
+        st.info("Sources for answering (2): \n" + st.session_state.sources[-1][1].page_content)
+
+with st.form("new_question", clear_on_submit=True):
+    q_submitted = st.form_submit_button("Ask another question", disabled=not q_submitted)
